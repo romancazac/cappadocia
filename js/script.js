72e6bbab57d7f738c75537e706b94a6bdeeb2ed5
romancazac <70430456+romancazac@users.noreply.github.com>
2020-09-24 13:04:21 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$('.slider,.sliderr').slick({
+		
+		dots:false,
+		slidesToShow:1,
+		autoplay:false,
+		speed:1000,
+		autoplaySpeed:800,
+		prevArrow:'<button type="button" class="slick-prev arrow arrow1"><img src="./img/arrowprev.svg" alt=""></button>',
+		nextArrow:'<button type="button" class="slick-next arrow arrow2"><img src="./img/n.svg" alt=""></button>',
+		
+		
+	});
+	
+    
+	
+});
